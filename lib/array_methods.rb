88a53440bdec98d29c9f63ae61fb4e6d7d29f758
Will Diep <widiep@gmail.com>
2019-09-13 09:12:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,20 +1,20 @@
-def find_element_index(array, value_to_find)
-  array.length.times do |count|
-    if array[count] == value_to_find
-      return count
-    end
-  end
-  nil
-end
-
 # def find_element_index(array, value_to_find)
-# 	array.each_with_index do |v, i|
-# 		if v == value_to_find
-# 			return i
-# 		end
-# 	end
-# 	return nil
+#   array.length.times do |count|
+#     if array[count] == value_to_find
+#       return count
+#     end
+#   end
+#   nil
 # end
+
+def find_element_index(array, value_to_find)
+	array.each_with_index do |v, i|
+		if v == value_to_find
+			return i
+		end
+	end
+	return nil
+end
 
 # def find_max_value(array)
 # 	max  = 0
